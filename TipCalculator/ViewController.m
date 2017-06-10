d3de86428a3670ca785e37ee3492806b957eaea6
nekomeo <linh.tu545@gmail.com>
2017-06-09 17:08:10 -0700

    Fix tip amount text field

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -11,7 +11,11 @@
 @interface ViewController () <UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UITextField *tipAmountTextField;
 @property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
+@property (weak, nonatomic) IBOutlet UISlider *tipSlider;
+@property (weak, nonatomic) IBOutlet UILabel *tipPercentLabel;
+@property (weak, nonatomic) IBOutlet UIButton *calculateTipButton;
 
 @end
 
@@ -29,13 +33,34 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)calculateTipButton:(UIButton *)sender {
+- (IBAction)billAmountTextField:(UITextField *)sender
+{
+    float billAmount = [sender.text floatValue];
+    float tip = (billAmount * self.tipSlider.value) / 100;
+    
+    NSString *tipFormatted = [NSString stringWithFormat:@"%.2f", tip];
+    self.tipAmountLabel.text = [@"$" stringByAppendingString:tipFormatted];
+}
+- (IBAction)calculateTipButton:(UIButton *)sender
+{
     float billAmount = [self.billAmountTextField.text floatValue];
-    float tipAmount = ((billAmount * 15)/100);
+    float tipAmountFloat = [self.tipAmountTextField.text floatValue];
+    float tipAmount = (billAmount * tipAmountFloat) / 100;
     NSString *tipString = [NSString stringWithFormat:@"%.2f", tipAmount];
     self.tipAmountLabel.text = [NSString stringWithFormat:@"Tip amount: $%.2f", tipAmount];
     
-    NSLog(@"Bill $%.2f at 15 percent is $%@", billAmount, tipString);
+    NSLog(@"Bill amount $%.2f at %@%% tip", billAmount, tipString);
+}
+
+
+- (IBAction)tipSlider:(UISlider *)sender
+{
+    float billAmount = [self.billAmountTextField.text floatValue];
+    float tip = (billAmount * sender.value) / 100;
+    
+    NSString *tipFormatted = [NSString stringWithFormat:@"%.2f", tip];
+    self.tipAmountLabel.text = [@"$" stringByAppendingString:tipFormatted];
+    self.tipPercentLabel.text = [[NSString stringWithFormat:@"%.2f", sender.value] stringByAppendingString:@"%"];
 }
 
 @end
